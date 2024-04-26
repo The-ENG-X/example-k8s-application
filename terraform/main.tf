89ab8adb4ff7c336d8874c77ef4aa00a7b1fecc3
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-04-26 15:02:35 +0200

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,7 +80,7 @@ resource "null_resource" "update_kubeconfig" {
   
 }
 
-resource "helm_release" "ingress_ngix" {
+resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart = "ingress-nginx"
